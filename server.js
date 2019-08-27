fe1a405422b7b183341823cf63be5d9a39c9e099
Yashwanth <yn185013@ncr.com>
2019-08-27 17:31:18 +0530

    Slight modification in server.js files and adding getusers service

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+
+//set pprt
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+
+//routes
+
+app.get('/', function(req,res)
+{
+	res.render("index");
+});
+
+
+app.listen(port, function(){
+	
+	console.log("listening");
+});
+
+
+app.get('/getusers', (req, res) => {
+
+    var fs = require('fs');
+    
+    let rawdata = fs.readFileSync('users.json');
+    let data = JSON.parse(rawdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+    
+});
