54641b5c3ceee2212fb419e502659580281e1a22
Yashwanth <yn185013@ncr.com>
2019-08-28 10:43:39 +0530

    Adding sample end points to add user and emergency - editing emergeny html

--- server.js
+++ server.js
@@ -32,3 +32,78 @@ app.get('/getusers', (req, res) => {
     res.send(data);
     
 });
+
+app.get('/getemergencies', (req, res) => {
+
+    var fs = require('fs');
+    
+    let rawdata = fs.readFileSync('emergency.json');
+    let data = JSON.parse(rawdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+    
+});
+
+
+app.get('/addemergency', (req, res) => {
+	   res.setHeader('Content-Type', 'application/json');
+   // res.send(newUser);
+    res.send('Ok');
+});
+
+app.get('/adduser', (req, res) => {
+
+    var fs = require('fs');
+	const file = 'users.json';
+    
+    let rawdata = fs.readFileSync(file);
+    let usersData = JSON.parse(rawdata);
+	
+	//get data from query string
+	var query = req.query;
+    var casId = query['caseId'];
+	var uName = query['userName'];
+    var name = query['name'];
+    var number = query['number'];
+    var eNumber = query['emergencynum'];
+	var address = query['address'];
+	
+    // let newUser = {
+            // caseId: casId,
+			// userName: uName,
+            // name: name,
+            // personalNumber: number,
+            // emergencyNumber: eNumber,
+            // Address: address,
+        // };
+	
+	// //check of the record exists with username or caseId
+	// var lodash = require('lodash');
+	// var user = lodash.where(usersData, function (e) { return e.caseId === casId; });
+	// if(isEmpty(obj))
+	// {
+		// //add user
+		// userData.push(newUser);
+		
+		// var newData = JSON.stringify(userData, null, 2);
+		// fs.writeFile(file, newData, err => {
+			// if (err) console.error(err);
+			// else console.log('done');
+		// });
+		
+		
+	// }
+	
+    res.setHeader('Content-Type', 'application/json');
+   // res.send(newUser);
+    res.send('Ok');
+    
+});
+
+function isEmpty(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
